1a1cfc8659b23204f102817bc668ce1b3d19e3c8
mkama <kamalimaduddin@gmail.com>
2024-11-06 16:20:37 +0700

    minor fixes

--- utils/db.js
+++ utils/db.js
@@ -5,13 +5,6 @@ const pool = mysql.createPool({
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
 });
 
 module.exports = pool.promise();
